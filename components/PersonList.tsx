c0e6ebdbb8aab740b858a265dc943997110bd35c
jiisloth <joonas.val@gmail.com>
2025-11-15 20:00:05 +0200

    fix playername spacing

--- components/PersonList.tsx
+++ components/PersonList.tsx
@@ -52,7 +52,7 @@ export const PersonList = ({
       <div className={style.personList} style={{display: "flex", flexDirection: "row", flexWrap: 'wrap'}}>
         {
           Personlist.map((p, k) => {
-            return [...(k != 0 ? [" & "] : []), <PersonDisplay key={k} name={p.displayName} role={role}/>]
+            return [...(k != 0 ? [<>&nbsp;&&nbsp;</>] : []), <PersonDisplay key={k} name={p.displayName} role={role}/>]
           })
         }
       </div>
