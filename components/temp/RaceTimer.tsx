caac62acc3a8b389c64ecdabe35c0da50a1406ec
jiisloth <joonas.val@gmail.com>
2025-11-25 23:35:36 +0200

    fix racetimers

--- components/temp/RaceTimer.tsx
+++ components/temp/RaceTimer.tsx
@@ -25,14 +25,20 @@ export const TempRaceTimerDisplay = ({
   const diffEnd = timer.end != null ? (totalTimer.end == null || Math.abs(timer.end - totalTimer.end) > 500) : totalTimer.end != null
 
 
-  let others_found = false
+  let ohtersDiffEnd = false
+  let othersDiffStart = false
   timers.filter((t) => others.includes(t.id)).forEach((t) => {
     const diffMinorEnd = timer.end != null ? (t.end == null || Math.abs(timer.end - t.end) > 100) : t.end != null
+    const diffMinorStart = timer.start != null && t.start != null && Math.abs(timer.start - t.start) > 100
     if (diffMinorEnd){
-      others_found = true
+      ohtersDiffEnd = true
+    }
+    if (diffMinorStart){
+      othersDiffStart = true
     }
   })
-  if (!others_found && !diffStart && !diffEnd) return null
+  const show = (othersDiffStart && timer.end == null && diffStart) || (ohtersDiffEnd && timer.end != null)
+  if (!show) return null
   return (
     <div className={masking.overlay} style={{position: 'absolute', fontSize: "2em", top: padding, right: padding, bottom: padding, left: padding, display: 'flex', flexDirection: 'column', justifyContent: corner == 'BL' || corner == 'BR' ? 'flex-end' : 'flex-start', alignItems: corner == 'BL' || corner == 'TL' ? 'flex-start' : 'flex-end'}}>
       <TimerDisplay start={timer.start} end={timer.end} id={id}/>
